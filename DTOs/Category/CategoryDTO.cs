a1aad33dcaad4a97e29fc6c1a806eae6fd0371f1
Tiago Leite <76402203+tiago-fml@users.noreply.github.com>
2025-05-04 12:50:35 +0100

    added Id field

--- DTOs/Category/CategoryDTO.cs
+++ DTOs/Category/CategoryDTO.cs
@@ -4,7 +4,7 @@ namespace expenses_api.DTOs.Category;
 
 public class CategoryDTO
 {
+    public Guid Id { get; set; }
     public string Description { get; set; }
-    
     public TransactionType Type { get; set; }
 }
