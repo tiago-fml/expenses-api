53ade4a30b5e262ab613e14e3b1847be8203dab6
Tiago Leite <76402203+tiago-fml@users.noreply.github.com>
2025-06-16 18:10:43 +0100

    Update TransactionUpdateDTO.cs

--- expenses-api/DTOs/Transaction/TransactionUpdateDTO.cs
+++ expenses-api/DTOs/Transaction/TransactionUpdateDTO.cs
@@ -2,5 +2,8 @@ namespace expenses_api.DTOs.Transaction;
 
 public class TransactionUpdateDTO
 {
-    
+    public required string Description { get; set; }
+    public decimal Value { get; set; }
+    public DateTimeOffset ExecutedAt { get; set; }
+    public Guid CategoryId { get; set; }
 }
